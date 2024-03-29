b235b26488c732b7ac7a36cbc0e633ce9b82eb71
zachdressing <zed_dressel@hotmail.com>
2024-03-29 09:47:55 -0700

    redeploy bait 2

--- src/DataServices/DataService.ts
+++ src/DataServices/DataService.ts
@@ -4,7 +4,7 @@ export const weatherCall = async (latitude:number, longitude:number) => {
     const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=imperial`)
     const weatherData = await weatherPromise.json();
     return weatherData;
-}
+} 
 
 export const foreCall = async (latitude:number, longitude:number) => {
     const forecastPromise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=imperial`)
